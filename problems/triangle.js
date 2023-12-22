9617c30f8f6e91ea1d7b9b2bfe7ebb07c50f4903
thomas <thomasarbaugh@gmail.com>
2023-12-21 16:59:51 -0800

    triangle

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,2 +1,50 @@
-// Your code here 
-console.log(typeof []);
+ class Triangle {
+    constructor(side1, side2, side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+    getPerimeter(){
+        return this.side1 + this.side2 + this.side3
+    }
+    hasValidSideLengths(){
+        let valid = true;
+        if (this.side1 + this.side2 < this.side3) valid = false;
+        if (this.side2 + this.side3 < this.side1) valid = false;
+        if (this.side3 + this.side1 < this.side2) valid = false;
+        return valid;
+    }
+    validate() {
+        if (this.hasValidSideLengths()) this.isValid = true;
+     else this.isValid = false
+    }
+
+}
+class Scalene extends Triangle {
+    constructor(side1, side2, side3){
+        super (side1, side2, side3)
+    }
+    isScalene() {
+        let valid = true;
+        if (this.side1 == this.side2 || this.side1 == this.side3 || this.side2 == this.side3) valid = false;
+        return valid;
+    }
+    validate(){
+        if (this.isScalene) this.isValidScalene = true;
+        else this.isValidScalene = false
+    }
+}
+class Isosceles extends Triangle {
+    constructor(side1, side2, side3){
+        super (side1, side2, side3)
+    }
+    isIsosceles(){
+        let valid = true;
+        if (this.side1 == this.side2 || this.side1 == this.side3 || this.side2 == this.side3 && )
+
+    }
+    validate(){
+
+    }
+}
+module.exports = {Triangle, Scalene, Isosceles}
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,43 @@
-// Your code here 
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies);
+
+const {Triangle, Scalene, Isosceles} = require('../problems/triangle');
+
+beforeEach(()=>{
+    triangle1 = new Triangle (3,3,5);
+    triangle2 = new Scalene (3,6,8);
+    triangle3 = new Isosceles (4,4,3)
+});
+
+describe('triangle ()', function () {
+    it('side one through three are set', () => {
+        expect(triangle1.side1).to.equal(3);
+        expect(triangle1.side2).to.equal(3);
+        expect(triangle1.side3).to.equal(5);
+    });
+    it ('returns corect perimiter', () => {
+        expect(triangle1.getPerimeter()).to.equal(11);
+    });
+    it('checks that triangle is valid', () => {
+        expect(triangle1.hasValidSideLengths()).to.equal(true);
+    });
+    it ('sets isValid', () => {
+        triangle1.validate();
+        expect(triangle1.isValid).to.equal(true);
+    });
+    it('testing is scalene', () => {
+        expect(triangle2.isScalene()).to.equal(true);
+    });
+    it('testing scalene validity', () => {
+        triangle2.validate()
+        expect(triangle2.isValidScalene).to.equal(true);
+    });
+    it('testing is isosceles', () => {
+        expect(triangle2.isScalene()).to.equal(true);
+    });
+    it('testing isosceles validity', () => {
+        triangle2.validate()
+        expect(triangle2.isValidScalene).to.equal(true);
+});
